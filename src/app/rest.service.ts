09ef007bdcc6a98f3c69a7610bdae1272d4815bf
lonalore <lonalore@freemail.hu>
2017-05-19 14:14:47 +0200

    Initial commit.

--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -3,6 +3,7 @@ import {Http, Response} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 
 import {CoreService} from './core.service';
+import {ModuleService} from './module.service';
 
 // Import RxJs required methods
 import 'rxjs/add/operator/map';
@@ -12,14 +13,62 @@ import 'rxjs/add/operator/catch';
 export class RestService {
 
   constructor(private core: CoreService,
+              private module: ModuleService,
               private http: Http) {
+  }
+
+  /**
+   * Gets the X-CSRF-Token from Drupal.
+   * @returns {Observable<R|T>}
+   */
+  token() {
+    return this.requestToken()
+      .map((response: Response) => {
+        if (response.status === 200) {
+          const body = response.json();
+          return body.data || body;
+        }
+      })
+      .catch((error: any) => Observable.throw(error.json().message || 'Server error'));
+  }
+
+  /**
+   * Connects to Drupal and sets the currentUser object.
+   * @returns {Observable<R|T>}
+   */
+  connect() {
+    return this.requestConnect()
+      .map((response: Response) => {
+        if (response.status === 200) {
+          const body = response.json();
+          return body.data || body;
+        }
+      })
+      .catch((error: any) => Observable.throw(error.json().message || 'Server error'));
+  }
+
+  /**
+   * Logs into Drupal.
+   * @param name
+   * @param pass
+   * @returns {Observable<R|T>}
+   */
+  userLogin(name: string, pass: string) {
+    return this.requestUserLogin(name, pass)
+      .map((response: Response) => {
+        if (response.status === 200) {
+          const body = response.json();
+          return body.data || body;
+        }
+      })
+      .catch((error: any) => Observable.throw(error.json().message || 'Server error'));
   }
 
   /**
    * Gets the X-CSRF-Token from Drupal.
    * @returns {Observable<Response>}
    */
-  token() {
+  requestToken() {
     return this.http.get(this.core.restPath() + 'rest/session/token');
   }
 
@@ -27,7 +76,7 @@ export class RestService {
    * Connects to Drupal.
    * @returns {Observable<Response>}
    */
-  connect() {
+  requestConnect() {
     return this.http.get(this.core.restPath() + 'cm/connect?_format=json');
   }
 
@@ -37,7 +86,7 @@ export class RestService {
    * @param pass
    * @returns {Observable<Response>}
    */
-  userLogin(name: string, pass: string) {
+  requestUserLogin(name: string, pass: string) {
     return this.http.post(this.core.restPath() + 'user/login?_format=json', {name: name, pass: pass});
   }
 
