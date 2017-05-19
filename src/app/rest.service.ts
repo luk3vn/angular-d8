0a6667841d68bf491d298c29c14037da693843b4
lonalore <lonalore@freemail.hu>
2017-05-19 14:09:27 +0200

    Initial commit.

--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -3,7 +3,6 @@ import {Http, Response} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 
 import {CoreService} from './core.service';
-import {ModuleService} from './module.service';
 
 // Import RxJs required methods
 import 'rxjs/add/operator/map';
@@ -13,55 +12,33 @@ import 'rxjs/add/operator/catch';
 export class RestService {
 
   constructor(private core: CoreService,
-              private module: ModuleService,
               private http: Http) {
   }
 
   /**
    * Gets the X-CSRF-Token from Drupal.
-   * @returns {Observable<R|T>}
+   * @returns {Observable<Response>}
    */
   token() {
-    return this.http.get(this.core.restPath() + 'rest/session/token')
-      .map((response: Response) => {
-        if (response.status === 200) {
-          const body = response.json();
-          return body.data || body;
-        }
-      })
-      .catch((error: any) => Observable.throw(error.json().message || 'Server error'));
+    return this.http.get(this.core.restPath() + 'rest/session/token');
   }
 
   /**
-   * Connects to Drupal and sets the currentUser object.
-   * @returns {Observable<R|T>}
+   * Connects to Drupal.
+   * @returns {Observable<Response>}
    */
   connect() {
-    return this.http.get(this.core.restPath() + 'cm/connect?_format=json')
-      .map((response: Response) => {
-        if (response.status === 200) {
-          const body = response.json();
-          return body.data || body;
-        }
-      })
-      .catch((error: any) => Observable.throw(error.json().message || 'Server error'));
+    return this.http.get(this.core.restPath() + 'cm/connect?_format=json');
   }
 
   /**
-   * Logs into Drupal, then makes a jDrupal.connect() call to properly set the currentUser object.
+   * Logs into Drupal.
    * @param name
    * @param pass
-   * @returns {Observable<R|T>}
+   * @returns {Observable<Response>}
    */
   userLogin(name: string, pass: string) {
-    return this.http.post(this.core.restPath() + 'user/login?_format=json', {name: name, pass: pass})
-      .map((response: Response) => {
-        if (response.status === 200) {
-          const body = response.json();
-          return body.data || body;
-        }
-      })
-      .catch((error: any) => Observable.throw(error.json().message || 'Server error'));
+    return this.http.post(this.core.restPath() + 'user/login?_format=json', {name: name, pass: pass});
   }
 
 }
