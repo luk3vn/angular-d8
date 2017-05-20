795a1fbf51392ef3fe9ffd74a58c5ef6176fcc25
Lóna Lore <lonalore@freemail.hu>
2017-05-20 08:21:03 +0200

    Removed some unused files.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
+
 import {DrupalService, SITE_PATH, BASE_PATH} from './drupal.service';
 
 @NgModule({
